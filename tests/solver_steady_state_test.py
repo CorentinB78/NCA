0629672e73e6041e49d1bc4112a85d2c44d66a41
Corentin Bertrand <cbertrand@flatironinstitute.org>
2021-11-12 15:10:48 -0500

    Tests self-energy

--- tests/solver_steady_state_test.py
+++ tests/solver_steady_state_test.py
@@ -1,6 +1,7 @@
 import unittest
 from matplotlib import pyplot as plt
 import numpy as np
+from numpy.testing._private.utils import assert_equal
 from nca.utilities import *
 from nca.hybridizations import *
 from nca.solver_steady_state import *
@@ -68,6 +69,120 @@ class SolverSteadyStateTest(unittest.TestCase):
             G_grea_w[mask] / dos_w[mask] / (-2j * np.pi),
             tb.fermi(-freq_mesh.values()[mask], -Ef, beta),
             atol=1e-2,
+        )
+
+    def test_orbital_in_state(self):
+        H_loc = [0.0, 1.0, 1.0, 3.0]
+
+        time_mesh = Mesh(10.0, 101)
+        Delta = np.empty(101)
+
+        S = NCA_Steady_State_Solver(
+            H_loc,
+            {0: Delta, 1: Delta},
+            {0: Delta, 1: Delta},
+            time_mesh,
+        )
+
+        # orbitals: 0 = up, 1 = down,
+        # states: 0 = empty, 1 = up, 2 = down, 3 = both
+
+        self.assertEqual(S.is_orb_in_state(0, 0), False)
+        self.assertEqual(S.is_orb_in_state(1, 0), False)
+        self.assertEqual(S.is_orb_in_state(0, 1), True)
+        self.assertEqual(S.is_orb_in_state(1, 1), False)
+        self.assertEqual(S.is_orb_in_state(0, 2), False)
+        self.assertEqual(S.is_orb_in_state(1, 2), True)
+        self.assertEqual(S.is_orb_in_state(0, 3), True)
+        self.assertEqual(S.is_orb_in_state(1, 3), True)
+
+    def test_self_energy(self):
+        H_loc = [0.0, 1.0, 1.0, 3.0]
+        time_mesh = Mesh(10.0, 101)
+        times = time_mesh.values()
+        Delta_less_up = np.cos(3.0 * times) * np.sin(0.1 * times + 5.0)
+        Delta_grea_up = np.cos(4.0 * times) * np.sin(0.2 * times + 2.0)
+        Delta_less_dn = np.cos(5.0 * times) * np.sin(0.6 * times - 1.0)
+        Delta_grea_dn = np.cos(6.0 * times) * np.sin(0.5 * times + 3.0)
+
+        S = NCA_Steady_State_Solver(
+            H_loc,
+            {0: Delta_less_up, 1: Delta_less_dn},
+            {0: Delta_grea_up, 1: Delta_grea_dn},
+            time_mesh,
+        )
+
+        S.R_grea_reta[:, 0] = np.sin(5.0 * times) * np.cos(0.6 * times - 1.0)
+        S.R_grea_reta[:, 1] = np.sin(2.0 * times) * np.cos(0.3 * times - 4.0)
+        S.R_grea_reta[:, 2] = np.sin(7.0 * times) * np.cos(0.2 * times - 3.0)
+        S.R_grea_reta[:, 3] = np.sin(1.0 * times) * np.cos(0.5 * times - 9.0)
+
+        S.self_energy_grea()
+
+        # states: 0 = empty, 1 = up, 2 = down, 3 = both
+
+        np.testing.assert_array_almost_equal(
+            S.S_grea_reta[:, 0],
+            -1j * Delta_less_up[::-1] * S.R_grea_reta[:, 1]
+            - 1j * Delta_less_dn[::-1] * S.R_grea_reta[:, 2],
+            10,
+        )
+
+        np.testing.assert_array_almost_equal(
+            S.S_grea_reta[:, 1],
+            1j * Delta_grea_up * S.R_grea_reta[:, 0]
+            - 1j * Delta_less_dn[::-1] * S.R_grea_reta[:, 3],
+            10,
+        )
+
+        np.testing.assert_array_almost_equal(
+            S.S_grea_reta[:, 2],
+            1j * Delta_grea_dn * S.R_grea_reta[:, 0]
+            - 1j * Delta_less_up[::-1] * S.R_grea_reta[:, 3],
+            10,
+        )
+
+        np.testing.assert_array_almost_equal(
+            S.S_grea_reta[:, 3],
+            1j * Delta_grea_dn * S.R_grea_reta[:, 1]
+            + 1j * Delta_grea_up * S.R_grea_reta[:, 2],
+            10,
+        )
+
+        S.R_less[:, 0] = np.sin(5.0 * times) * np.cos(0.6 * times - 1.0)
+        S.R_less[:, 1] = np.sin(2.0 * times) * np.cos(0.3 * times - 4.0)
+        S.R_less[:, 2] = np.sin(7.0 * times) * np.cos(0.2 * times - 3.0)
+        S.R_less[:, 3] = np.sin(1.0 * times) * np.cos(0.5 * times - 9.0)
+
+        S.self_energy_less()
+
+        # states: 0 = empty, 1 = up, 2 = down, 3 = both
+
+        np.testing.assert_array_almost_equal(
+            S.S_less[:, 0],
+            1j * Delta_grea_up[::-1] * S.R_less[:, 1]
+            + 1j * Delta_grea_dn[::-1] * S.R_less[:, 2],
+            10,
+        )
+
+        np.testing.assert_array_almost_equal(
+            S.S_less[:, 1],
+            -1j * Delta_less_up * S.R_less[:, 0]
+            + 1j * Delta_grea_dn[::-1] * S.R_less[:, 3],
+            10,
+        )
+
+        np.testing.assert_array_almost_equal(
+            S.S_less[:, 2],
+            -1j * Delta_less_dn * S.R_less[:, 0]
+            + 1j * Delta_grea_up[::-1] * S.R_less[:, 3],
+            10,
+        )
+
+        np.testing.assert_array_almost_equal(
+            S.S_less[:, 3],
+            -1j * Delta_less_dn * S.R_less[:, 1] - 1j * Delta_less_up * S.R_less[:, 2],
+            10,
         )
 
 
